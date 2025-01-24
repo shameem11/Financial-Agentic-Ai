39f6780533f5da44ed54bc513bacd63b701e20d1
shameem11 <shameemmon.mk@gmail.com>
2025-01-24 19:17:24 +0400

    Agent

---
+++ Financial_Agent.py
@@ -0,0 +1,46 @@
+
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.tools.yfinance import YFinanceTools
+
+
+# web search 
+
+
+from dotenv import load_dotenv
+import os
+
+web_agent = Agent(
+    name="Web Agent",
+    role="Search the web for information",
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[DuckDuckGo()],
+    instructions=["Always include sources"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+# Financial Agent
+
+finance_agent = Agent(
+    name="Finance Agent",
+    role="Get financial data",
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[YFinanceTools(stock_price=True,
+                         analyst_recommendations=True,
+                         company_info=True)],
+    instructions=["Use tables to display data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+
+agent_team = Agent(
+    team=[web_agent, finance_agent],
+    instructions=["Always include sources", "Use tables to display data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+agent_team.print_response("Summarize analyst recommendations and share the latest news for Apple.lnc", stream=True)
